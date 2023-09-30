29dbd2f763bda71eabd2f31ed175e7b365f55d84
yuvalramat <yuvalazulai2016@gmail.com>
2023-09-30 16:11:56 +0300

    s

--- JS/main.js
+++ JS/main.js
@@ -2,7 +2,7 @@ document.getElementById("myBody").innerHTML = `
 <header id="header"></header>
 `   
 const pages = ["Home", "Movies", "Searching", "About", "Favorite"];
-const aHrefs = ["/index.html", "movieId.html", "searching.html", "UsAbout.html", "favorite.html"];
+const aHrefs = ["index.html", "movieId.html", "searching.html", "UsAbout.html", "favorite.html"];
 for (let i = 0; i < 5; i++) {
         header.innerHTML += `
         <a href="${aHrefs[i]}">${pages[i]}</a>
